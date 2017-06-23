1c92ae5e5c84504ee5006b07d67686d43de210be
javieranselmi <javieranselmi@gmail.com>
2017-06-23 18:27:35 -0300

    database

--- config/knex.js
+++ config/knex.js
@@ -5,9 +5,9 @@ var dbConfig = {
     client: 'mysql',
     debug: false,
     connection: {
-        host     : 'db',
+        host     : 'neuron.c7cblth0trpq.us-east-1.rds.amazonaws.com',
         user     : 'neuron',
-        password : 'neuron',
+        password : 'zapata12',
         database : 'neuron',
         charset   : 'utf8',
     } //TODO ADD to .env
