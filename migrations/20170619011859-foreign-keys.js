2b5382d6bc9233ae02d9a82a025de69bf4fcb923
javieranselmi <javieranselmi@gmail.com>
2017-06-24 12:43:24 -0300

    repair migrations

--- migrations/20170619011859-foreign-keys.js
+++ migrations/20170619011859-foreign-keys.js
@@ -5,47 +5,60 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
 };
 
-exports.up = function(db) {
+exports.up = function (db) {
     var dbActions = [];
 
     dbActions.push(db.addForeignKey('sessions', 'professionals', 'sessions_professionals_foreign_key',
-        { 'professional_id': 'id' },
-        { onUpdate: 'RESTRICT' }));
+        {'professional_id': 'id'},
+        {
+            onUpdate: 'RESTRICT',
+            onDelete: 'RESTRICT'
+        }));
 
     dbActions.push(db.addForeignKey('sessions', 'patients', 'sessions_patients_foreign_key',
-        { 'patient_id': 'id' },
-        { onUpdate: 'RESTRICT' }));
+        {'patient_id': 'id'},
+        {
+            onUpdate: 'RESTRICT',
+            onDelete: 'RESTRICT'
+        }));
 
-    dbActions.push(db.addForeignKey('sessions_services', 'sessions', 'sessions_services_sessions_foreign_key',
-        { 'session_id': 'id' },
-        { onUpdate: 'RESTRICT' }));
+    dbActions.push(db.addForeignKey('services_sessions', 'sessions', 'sessions_services_sessions_foreign_key',
+        {'session_id': 'id'}
+        , {
+            onUpdate: 'RESTRICT',
+            onDelete: 'RESTRICT'
+        }));
 
-    dbActions.push(db.addForeignKey('sessions_services', 'services', 'sessions_services_services_foreign_key',
-        { 'service_id': 'id' },
-        { onUpdate: 'RESTRICT' }));
+    dbActions.push(db.addForeignKey('services_sessions', 'services', 'sessions_services_services_foreign_key',
+        {'service_id': 'id'}
+        ,
+        {
+            onUpdate: 'RESTRICT',
+            onDelete: 'RESTRICT'
+        }));
 
 
-  return null;
+    return Promise.all(dbActions);
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
     return Promise.all([
-        db.dropTable('sessions_professionals_foreign_key'),
-        db.dropTable('sessions_patients_foreign_key'),
-        db.dropTable('sessions_services'),
-        db.dropTable('sessions_services_sessions_foreign_key'),
+        db.removeForeignKey('services_sessions','sessions_services_sessions_foreign_key'),
+        db.removeForeignKey('services_sessions','sessions_services_services_foreign_key'),
+        db.removeForeignKey('sessions','sessions_patients_foreign_key'),
+        db.removeForeignKey('sessions','sessions_professionals_foreign_key'),
     ]);
 };
 
 exports._meta = {
-  "version": 1
+    "version": 1
 };
